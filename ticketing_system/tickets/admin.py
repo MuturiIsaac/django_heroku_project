3a724df2aa2c29a695713b08d13c2bd07c118ad9
MuturiIsaac <mbuguaisaac68@gmail.com>
2024-05-02 23:58:32 +0300

    admin

--- ticketing_system/tickets/admin.py
+++ ticketing_system/tickets/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Ticket, UserProfile
+from django.contrib.auth.models import User
 
 # Register your models here.
+admin.site.register(Ticket)
+admin.site.register(UserProfile)
+
+# Add the following to display additional user information in the admin interface
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
