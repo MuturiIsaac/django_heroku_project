a34645e53b17c75074a93450564827bb4d8210f5
MuturiIsaac <mbuguaisaac68@gmail.com>
2024-05-02 23:57:39 +0300

    userprofile model

--- ticketing_system/tickets/models.py
+++ ticketing_system/tickets/models.py
@@ -19,3 +19,10 @@ class Ticket(models.Model):
 
     def __str__(self):
         return self.title
+    
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    is_staff = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
